62b528454863b9d4bcf0b6d984d0c01907221817
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-06-19 15:09:07 +0200

    EX: 6.1

--- FUNDAMENTOS/BLOCO-6-HTML-E-CSS-FORMS/DIA-1-FORMS/viagem/script.js
+++ FUNDAMENTOS/BLOCO-6-HTML-E-CSS-FORMS/DIA-1-FORMS/viagem/script.js
@@ -1,32 +1,27 @@
 //const INPUT_TEXT = document.querySelector("#enviar");
 
-
-function handleSubmit(event) {
-    event.preventDefault();
-    const validation = textInputValidation();
-    if (validation === false) {
-      alert('Dados inválidos');
-    } else {
-      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-    }
-  }
-
-
 //function handleSubmit(event) {
   //  event.preventDefault();
  // }
 
+ /*
 window.onload = function () {
     const INPUT_TEXT = document.querySelector('#enviar');
     INPUT_TEXT.addEventListener('click', handleSubmit);
   };
-
+*/
 //INPUT_TEXT.addEventListener("click", function(event){
   //  event.preventDefault()
   //});
 
-
-
+  /*
+  window.onload = function () {
+    const clearBtn = document.querySelector('#apagar');
+    clearBtn.addEventListener('click', clearFields);
+    const submitBtn = document.querySelector('#enviar');
+    submitBtn.addEventListener('click', handleSubmit);
+  };
+*/
 
   //4
   function clearFields() {
@@ -40,38 +35,23 @@ window.onload = function () {
     textArea.value = '';
   }
 
-  window.onload = function () {
-    const clearBtn = document.querySelector('#apagar');
-    clearBtn.addEventListener('click', clearFields);
-    const submitBtn = document.querySelector('#enviar');
-    submitBtn.addEventListener('click', handleSubmit);
-  };
 
-
-  //bonus
+  //bonus 1
   function enableSubmit() {
     const submitBtn = document.querySelector('#enviar');
     const agreement = document.querySelector('#agreement');
     submitBtn.disabled = !agreement.checked;
   }
 
-  window.onload = function() {
-    const clearBtn = document.querySelector('#apagar');
-    clearBtn.addEventListener('click', clearFields);
-    const submitBtn = document.querySelector('#enviar');
-    submitBtn.addEventListener('click', handleSubmit);
-    const agreement = document.querySelector('#agreement');
-    agreement.addEventListener('change', enableSubmit);
-  };
-
 
 //bonus 2
   function textInputValidation() {
-    const email = document.querySelector('#email-completo').value.length;
-    const invalidEmail = email < 10 || email > 50;
-  
+
     const name = document.querySelector('#nome-completo').value.length;
     const invalidName = name < 10 || name > 40;
+
+    const email = document.querySelector('#email-completo').value.length;
+    const invalidEmail = email < 10 || email > 50;
   
     const reason = document.querySelector('#texto-1').value.length;
     const invalidReason = reason > 500;
@@ -84,3 +64,25 @@ window.onload = function () {
     }
   }
 
+
+  //exe 3 e bonus 2
+  function handleSubmit(event) {
+    event.preventDefault();
+    const validation = textInputValidation();
+    if (validation === false) {
+      alert('Dados inválidos');
+    } else {
+      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
+  }
+
+
+  //exe 3 - exe 4 - bonus 1
+  window.onload = function() {
+    const submitBtn = document.querySelector('#enviar');
+    submitBtn.addEventListener('click', handleSubmit);
+    const clearBtn = document.querySelector('#apagar');
+    clearBtn.addEventListener('click', clearFields);
+    const agreement = document.querySelector('#agreement');
+    agreement.addEventListener('change', enableSubmit);
+  };
